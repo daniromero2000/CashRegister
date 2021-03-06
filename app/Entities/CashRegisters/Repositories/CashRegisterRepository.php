3b51ab503af67ff8d9e986c8d8d02e6c1f34f11e
Daniel Romero <123romerod@gmail.com>
2021-03-05 20:16:56 -0500

    Daniel, Feature/createEntitieCashRegister, Mar 5/2021. Se crea repositorio, interfaz y relacion para el modelo CashRegister

--- app/Entities/CashRegisters/Repositories/CashRegisterRepository.php
+++ app/Entities/CashRegisters/Repositories/CashRegisterRepository.php
@@ -1,10 +1,42 @@
 <?php
 
-
 namespace App\Entities\CashRegisters\Repositories;
 
+use App\Entities\CashRegisters\CashRegister;
+use App\Entities\CashRegisters\Repositories\Interfaces\CashRegisterRepositoryInterface;
 
-class CashRegisterRepository
+class CashRegisterRepository implements CashRegisterRepositoryInterface
 {
+    /**
+     * @var CashRegister
+     */
+    protected $cashRegister;
+
+    /**
+     * CashRegisterRepository constructor.
+     * @param CashRegister $cashRegister
+     */
+    public function __construct(CashRegister $cashRegister)
+    {
+        $this->cashRegister = $cashRegister;
+    }
+
+    /**
+     * @param array|string[] $columns
+     * @return array
+     */
+    public function listCashRegisters(array $columns = ['*']): array
+    {
+        return $this->cashRegister->get($columns);
+    }
+
+    /**
+     * @param array $data
+     * @return CashRegister
+     */
+    public function createCashRegister(array $data): CashRegister
+    {
+        return $this->cashRegister->create($data);
+    }
 
 }
--- app/Entities/CashRegisters/Repositories/Interfaces/CashRegisterRepositoryInterface.php
+++ app/Entities/CashRegisters/Repositories/Interfaces/CashRegisterRepositoryInterface.php
@@ -1,10 +1,24 @@
 <?php
 
-
 namespace App\Entities\CashRegisters\Repositories\Interfaces;
 
+use App\Entities\CashRegisters\CashRegister;
 
+/**
+ * Interface CashRegisterRepositoryInterface
+ * @package App\Entities\CashRegisters\Repositories\Interfaces
+ */
 interface CashRegisterRepositoryInterface
 {
+    /**
+     * @param array|string[] $columns
+     * @return array
+     */
+    public function listCashRegisters(array $columns = ['*']): array;
 
+    /**
+     * @param array $data
+     * @return CashRegister
+     */
+    public function createCashRegister(array $data): CashRegister;
 }
