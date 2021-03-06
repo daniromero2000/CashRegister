2d9368ff1666b9f1b1f5b43ab580c751d8c97d0a
Daniel Romero <123romerod@gmail.com>
2021-03-05 19:36:49 -0500

    Daniel, Feature/createClassTransactionLog, Mar 5/2021, se crea modelo para la tabla transaction_logs

--- app/Entities/TransactionLogs/TransactionLog.php
+++ app/Entities/TransactionLogs/TransactionLog.php
@@ -1,10 +1,55 @@
 <?php
 
-
 namespace App\Entities\TransactionLogs;
 
+use Illuminate\Database\Eloquent\Model;
 
-class TransactionLog
+/**
+ * Class TransactionLog
+ * @package App\Entities\TransactionLogs
+ * @author Daniel Romero - 123romerod@gmail.com
+ */
+class TransactionLog extends Model
 {
+    /**
+     * @var string
+     */
+    protected $table = 'transaction_logs';
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'id',
+        'type',
+        'value',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
 
 }
